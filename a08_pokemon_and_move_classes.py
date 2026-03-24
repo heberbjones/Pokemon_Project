71d99bb695a6c4edebca268a29da4b14a8f5ea96
heberbjones <heberbjones@gmail.com>
2026-03-24 15:33:36 -0600

    Made the pokemon class

--- a08_pokemon_and_move_classes.py
+++ a08_pokemon_and_move_classes.py
@@ -6,5 +6,18 @@
 
 # Heber - Pokemon Classes
 
+class Pokemon :
+    def __init__(self, sName, sElemental_type, iHit_points):
+        self.Name = sName
+        self.Elemental_type = sElemental_type
+        self.Hit_points = iHit_points
+
+    def get_info (self) :
+        return f'Name: {self.Name} | Element Type: {self.Elemental_type} | Hit Points: {self.Hit_points}'   
+
+    def heal (self) :
+        self.Hit_points += 15
+        print(f'Updated Hit Points: {self.Hit_points}')     
+
 
 # Carl - Main program
